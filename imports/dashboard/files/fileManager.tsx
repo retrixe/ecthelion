retrixe/ecthelion

commit dcff78c0a361024277ab0714631c54a22f9e31e7

Explicit function return types are a good idea

imports/dashboard/files/fileManager.tsx
@@ -35,7 +35,7 @@ const editorExts = ['properties', 'json', 'yaml', 'yml', 'xml', 'js', 'log', 'sh
 const FileManager = (props: {
   setServerExists: React.Dispatch<React.SetStateAction<boolean>>
   setAuthenticated: React.Dispatch<React.SetStateAction<boolean>>
-}) => {
+}): JSX.Element => {
   const router = useRouter()
   const { server, node, ip } = useOctyneData() // nodeExists is handled above.
   const ky = useKy(node)
@@ -120,7 +120,7 @@ const FileManager = (props: {
 
   useEffect(() => {
     if (typeof window === 'undefined' || file) return
-    const eventListener = (e: KeyboardEvent) => {
+    const eventListener = (e: KeyboardEvent): void => {
       if (e.code === 'F3' || (e.ctrlKey && e.code === 'KeyF')) {
         e.preventDefault()
         searchRef.current?.focus()
@@ -174,7 +174,7 @@ const FileManager = (props: {
   }, [filename, files, path, updatePath, loadFileInEditor])
 
   // Multiple file logic requests.
-  const handleCreateFolder = async (name: string) => {
+  const handleCreateFolder = async (name: string): Promise<void> => {
     setFolderPromptOpen(false)
     setFetching(true)
     try {
@@ -188,17 +188,17 @@ const FileManager = (props: {
       setFetching(false)
     }
   }
-  const handleModifyFile = async (pathToMove: string, action: 'move' | 'copy' | 'rename') => {
+  const handleModifyFile = async (path: string, action: 'move' | 'copy' | 'rename'): Promise<void> => {
     setModifyFileDialogOpen('')
     setMenuOpen('')
     setAnchorEl(null)
     setFetching(true)
-    if (action === 'rename' && pathToMove.includes('/')) {
+    if (action === 'rename' && path.includes('/')) {
       setMessage('Renamed file cannot have / in it!')
       setFetching(false)
       return
     }
-    const target = action === 'rename' ? path + pathToMove : pathToMove
+    const target = action === 'rename' ? path + path : path
     try {
       const editFile = await ky.patch(`server/${server}/file`, {
         body: `${action === 'copy' ? 'cp' : 'mv'}\n${path}${menuOpen}\n${target}`
@@ -211,7 +211,7 @@ const FileManager = (props: {
       setFetching(false)
     }
   }
-  const handleFilesDelete = () => {
+  const handleFilesDelete = (): void => {
     setMassActionMenuOpen(null)
     let total = filesSelected.length
     setOverlay(`Deleting ${total} out of ${filesSelected.length} files.`)
@@ -233,7 +233,7 @@ const FileManager = (props: {
       fetchFiles()
     }).catch(console.error) // Should not be called, ideally.
   }
-  const handleFilesUpload = (files: FileList) => {
+  const handleFilesUpload = (files: FileList): void => {
     ;(async () => {
       for (let i = 0; i < files.length; i++) {
         const file = files[i]
@@ -252,7 +252,7 @@ const FileManager = (props: {
     })().catch(e => { console.error(e); setMessage(`Failed to upload files: ${e.message}`) })
   }
   // Single file logic.
-  const handleDeleteMenuButton = () => {
+  const handleDeleteMenuButton = (): void => {
     ;(async () => {
       setMenuOpen('')
       setFetching(true)
@@ -264,14 +264,14 @@ const FileManager = (props: {
       fetchFiles()
     })().catch(e => { console.error(e); setMessage(`Failed to delete file: ${e.message}`) })
   }
-  const handleDownloadMenuButton = () => {
+  const handleDownloadMenuButton = (): void => {
     ;(async () => {
       setMenuOpen('')
       const ticket = encodeURIComponent((await ky.get('ott').json<{ ticket: string }>()).ticket)
       window.location.href = `${ip}/server/${server}/file?ticket=${ticket}&path=${path}${menuOpen}`
     })().catch(e => { console.error(e); setMessage(`Failed to download file: ${e.message}`) })
   }
-  const handleDecompressMenuButton = () => {
+  const handleDecompressMenuButton = (): void => {
     ;(async () => {
       setMenuOpen('')
       setFetching(true)
@@ -285,8 +285,8 @@ const FileManager = (props: {
       fetchFiles()
     })().catch(e => { console.error(e); setMessage(`Failed to decompress file: ${e.message}`) })
   }
-  const handleCloseDownload = () => { setDownload(''); updatePath(path) }
-  const handleDownloadButton = () => {
+  const handleCloseDownload = (): void => { setDownload(''); updatePath(path) }
+  const handleDownloadButton = (): void => {
     ;(async () => {
       handleCloseDownload()
       // document.cookie = `X-Authentication=${localStorage.getItem('token')}`
@@ -295,7 +295,7 @@ const FileManager = (props: {
       window.location.href = loc
     })().catch((e: any) => { console.error(e); setMessage(`Failed to download file: ${e.message}`) })
   }
-  const handleSaveFile = async (name: string, content: string) => {
+  const handleSaveFile = async (name: string, content: string): Promise<void> => {
     try {
       const formData = new FormData()
       formData.append('upload', new Blob([content]), name)

.eslintrc.js
@@ -16,7 +16,6 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     // Make TypeScript ESLint less strict.
-    '@typescript-eslint/explicit-function-return-type': 'off', // Opt into this later?
     '@typescript-eslint/strict-boolean-expressions': 'off',
     '@typescript-eslint/triple-slash-reference': 'off',
     '@typescript-eslint/restrict-template-expressions': 'off',

imports/dashboard/console/consoleButtons.tsx
@@ -6,7 +6,7 @@ import PlayArrow from '@mui/icons-material/PlayArrow'
 
 const ConsoleButtons = ({ stopStartServer }: {
   stopStartServer: (operation: 'START' | 'TERM' | 'KILL') => void
-}) => {
+}): JSX.Element => {
   const smallScreen = useMediaQuery(useTheme().breakpoints.only('xs'))
   const [confirmingKill, setConfirmingKill] = useState(false)
 

imports/dashboard/console/consoleView.tsx
@@ -10,7 +10,7 @@ try {
   ) chrome = true
 } catch (e) {}
 
-const ChromeConsoleView = (props: { console: Array<{ id: number, text: string }> }) => {
+const ChromeConsoleView = (props: { console: Array<{ id: number, text: string }> }): JSX.Element => {
   return (
     <div
       style={{
@@ -31,7 +31,7 @@ const ChromeConsoleView = (props: { console: Array<{ id: number, text: string }>
   )
 }
 
-const ConsoleView = (props: { console: Array<{ id: number, text: string }> }) => {
+const ConsoleView = (props: { console: Array<{ id: number, text: string }> }): JSX.Element => {
   const ref = useRef<HTMLDivElement>(null)
   const isScrolledToBottom = ref.current !== null
     ? ref.current.scrollHeight - ref.current.clientHeight <= ref.current.scrollTop + 1

imports/dashboard/dashboardLayout.tsx
@@ -18,7 +18,7 @@ import Layout from '../layout'
 import config from '../config'
 import UnstyledLink from '../helpers/unstyledLink'
 
-const DrawerItem = (props: { icon: React.ReactElement, name: string, subUrl: string }) => {
+const DrawerItem = (props: { icon: React.ReactElement, name: string, subUrl: string }): JSX.Element => {
   const { server, node } = useRouter().query
   const nodeUri = typeof node === 'string' ? `?node=${encodeURIComponent(node)}` : ''
   return (
@@ -32,7 +32,7 @@ const DrawerItem = (props: { icon: React.ReactElement, name: string, subUrl: str
   )
 }
 
-const onLogout = () => {
+const onLogout = (): void => {
   const token = localStorage.getItem('token')
   if (token) {
     fetch(`${config.ip}/logout`, { headers: { Authorization: token } }).catch(console.error)
@@ -40,7 +40,7 @@ const onLogout = () => {
   }
 }
 
-const DashboardLayout = (props: React.PropsWithChildren<{ loggedIn: boolean }>) => {
+const DashboardLayout = (props: React.PropsWithChildren<{ loggedIn: boolean }>): JSX.Element => {
   const [openDrawer, setOpenDrawer] = useState(false)
   const drawerVariant = useMediaQuery(useTheme().breakpoints.only('xs')) ? 'temporary' : 'permanent'
   const appBarContent = (

imports/dashboard/files/editor.tsx
@@ -11,13 +11,13 @@ const Editor = (props: {
   onDownload: () => void
   onClose: (setContent: React.Dispatch<React.SetStateAction<string>>) => void
   closeText?: string
-}) => {
+}): JSX.Element => {
   const [content, setContent] = useState(props.content)
   const [saving, setSaving] = useState(false)
   const [name, setName] = useState(props.name)
   const error = props.name === '' && props.siblingFiles.includes(name)
 
-  const saveFile = () => {
+  const saveFile = (): void => {
     setSaving(true)
     Promise.resolve(props.onSave(name, content)).then(() => setSaving(false), console.error)
   }

imports/dashboard/files/fileList.tsx
@@ -12,15 +12,15 @@ import InsertDriveFile from '@mui/icons-material/InsertDriveFile'
 import UnstyledLink from '../../helpers/unstyledLink'
 import { joinPath } from './fileUtils'
 
-const rtd = (num: number) => Math.round(num * 100) / 100
-const bytesToGb = (bytes: number) => {
+const rtd = (num: number): number => Math.round(num * 100) / 100
+const bytesToGb = (bytes: number): string => {
   if (bytes < 1024) return `${bytes} bytes`
   else if (bytes < (1024 * 1024)) return `${rtd(bytes / 1024)} KB`
   else if (bytes < (1024 * 1024 * 1024)) return `${rtd(bytes / (1024 * 1024))} MB`
   else if (bytes < (1024 * 1024 * 1024 * 1024)) return `${rtd(bytes / (1024 * 1024 * 1024))} GB`
   else return `${rtd(bytes / (1024 * 1024 * 1024 * 1024))} TB`
 }
-const tsts = (ts: number) => new Date(ts * 1000).toISOString().substr(0, 19).replace('T', ' ')
+const tsts = (ts: number): string => new Date(ts * 1000).toISOString().substr(0, 19).replace('T', ' ')
 
 export interface File {
   name: string
@@ -39,7 +39,7 @@ const FileListItem = ({ file, style, disabled, filesSelected, onItemClick, onChe
   onCheck: React.MouseEventHandler<HTMLButtonElement>
   onItemClick: React.MouseEventHandler<HTMLDivElement>
   openMenu: (fileName: string, anchorEl: HTMLButtonElement) => void
-}) => (
+}): JSX.Element => (
   <UnstyledLink href={url} style={style} onClick={e => e.preventDefault()}>
     <ListItem
       key={file.name}
@@ -75,15 +75,15 @@ const FileListItem = ({ file, style, disabled, filesSelected, onItemClick, onChe
     </ListItem>
   </UnstyledLink>
 )
-const FileListItemRenderer = ({ index, data, style }: ListChildComponentProps) => {
+const FileListItemRenderer = ({ index, data, style }: ListChildComponentProps): JSX.Element => {
   const { files, path, disabled, filesSelected, setFilesSelected, openMenu, onClick } = data as FileItemData
   const router = useRouter()
   const file = files[index]
-  const selectItem = () => {
+  const selectItem = (): void => {
     if (!filesSelected.includes(file.name)) setFilesSelected([...filesSelected, file.name])
     else setFilesSelected(filesSelected.filter(e => e !== file.name))
   }
-  const shiftClickItem = () => {
+  const shiftClickItem = (): void => {
     // Look for the last selected item. If none found, look for closest item to the start.
     let lastSelectedFileIdx = files.findLastIndex(e => filesSelected.includes(e.name))
     if (lastSelectedFileIdx === -1) lastSelectedFileIdx = 0 // If none found, select first item.
@@ -122,7 +122,7 @@ interface FileItemData { /* eslint-disable react/no-unused-prop-types */
   onClick: (name: File) => void
 } /* eslint-enable react/no-unused-prop-types */
 
-const FileList = (props: FileItemData) => {
+const FileList = (props: FileItemData): JSX.Element => {
   const px60 = useMediaQuery('(min-width:713px)')
   const smDisplay = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm')) // 600px
   const px60sm = useMediaQuery('(min-width:513px)')

imports/dashboard/files/fileUtils.ts
@@ -1,16 +1,16 @@
-export const normalisePath = (path: string) => {
+export const normalisePath = (path: string): string => {
   const fixedPath = path.replace(/\/+/g, '/')
   return (fixedPath.startsWith('/') ? '' : '/') + fixedPath + (fixedPath.endsWith('/') ? '' : '/')
 }
 
-export const parentPath = (path: string) => {
+export const parentPath = (path: string): string => {
   const pathSegments = normalisePath(path).split('/')
   if (pathSegments.length < 3) return '/'
   pathSegments.splice(pathSegments.length - 2, 2)
   return pathSegments.join('/')
 }
 
-export const joinPath = (a: string, b: string) => {
+export const joinPath = (a: string, b: string): string => {
   const normalisedPathA = normalisePath(a)
   const normalisedPathB = normalisePath(b)
   return normalisePath(normalisedPathA + normalisedPathB)

commit d1edae11b438df56398e956c9b7906720223c006

Fix broken func arg regression from dcff78c.

imports/dashboard/files/fileManager.tsx
@@ -190,17 +190,17 @@ const FileManager = (props: {
       setFetching(false)
     }
   }
-  const handleModifyFile = async (path: string, action: 'move' | 'copy' | 'rename'): Promise<void> => {
+  const handleModifyFile = async (newPath: string, action: 'move' | 'copy' | 'rename'): Promise<void> => {
     setModifyFileDialogOpen('')
     setMenuOpen('')
     setAnchorEl(null)
     setFetching(true)
-    if (action === 'rename' && path.includes('/')) {
+    if (action === 'rename' && newPath.includes('/')) {
       setMessage('Renamed file cannot have / in it!')
       setFetching(false)
       return
     }
-    const target = action === 'rename' ? path + path : path
+    const target = action === 'rename' ? path + newPath : newPath
     try {
       const editFile = await ky.patch(`server/${server}/file`, {
         body: `${action === 'copy' ? 'cp' : 'mv'}\n${path}${menuOpen}\n${target}`