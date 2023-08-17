a91fc53d74737bc99afc7ed45a567032fe643a8e
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2023-08-17 23:44:20 +0530

    Migrate to Yarn v4 and PnP from v1.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,3 @@
-// well....
-if (require('eslint-config-standard-with-typescript/package.json').version === '37.0.0') {
-  console.log('Patching eslint-config-standard-with-typescript for TypeScript ESLint 6 support.')
-  const fs = require('fs')
-  const path = require('path')
-  const target = path.join(__dirname, 'node_modules', 'eslint-config-standard-with-typescript', 'lib', 'index.js')
-  const contents = fs.readFileSync(target, { encoding: 'utf8' }).split('\n')
-    .filter(line => !line.includes('restrict-plus-operands'))
-    .join('\n')
-  fs.writeFileSync(target, contents, { encoding: 'utf8' })
-}
-
 module.exports = {
   env: {
     es6: true,
