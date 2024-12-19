5dd82c419c7bdfb52e16639ab86863d9f460e2e1
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2024-12-20 00:34:37 +0530

    Move to Vitest from Jest

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,19 +1,12 @@
 import React from 'react'
+import { expect, test } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import Home from '../pages/index'
-import '@testing-library/jest-dom'
 
-jest.mock('next/router', () => require('next-router-mock'))
-jest.mock('next/dist/client/router', () => require('next-router-mock'))
-
-describe('Home', () => {
-  it('renders a heading', () => {
-    render(<Home />)
-
-    const heading = screen.getByRole('heading', {
-      name: /Octyne/i
-    })
-
-    expect(heading).toBeInTheDocument()
+test('renders a heading', () => {
+  render(<Home />)
+  const heading = screen.getByRole('heading', {
+    name: /Octyne/i
   })
+  expect(heading).toBeDefined()
 })
