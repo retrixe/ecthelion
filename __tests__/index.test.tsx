cb9d410074cfd791c67066e5bd29782b64100e0a
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2025-08-29 16:37:00 +0530

    Format JSON files, tests, use gitignore with Biome

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,6 +1,6 @@
+import { render, screen } from '@testing-library/react'
 import React from 'react'
 import { expect, test } from 'vitest'
-import { render, screen } from '@testing-library/react'
 import Home from '../pages/index'
 
 test('renders a heading', () => {
